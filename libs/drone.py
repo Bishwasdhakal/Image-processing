e9c089f4158316c1f4290fd57e69553fd53c2a43
Juan Pablo Enrique <enrique.juanpablo7@gmail.com>
2019-03-26 16:48:39 -0400

    cleaned up drone class

--- libs/drone.py
+++ libs/drone.py
@@ -14,14 +14,11 @@ class Drone:
     """Drone instance. Starts and flies drone"""
     logging.basicConfig(level=logging.ERROR)
 
+    def __init__(self, URI):
     """
     Init commad connects to drone
-    default height is 0.3m
-    default take-off velocity is 0.2
+    :param URI: URI
     """
-    def __init__(self, URI, height = 0.3, velocity = 0.2):
-        self.height = height
-        self.velocity = velocity
         self._cf = Crazyflie(rw_cache='./cache')
         cflib.crtp.init_drivers(enable_debug_driver=False)
         scf = SyncCrazyflie(URI, cf = self._cf)
@@ -29,26 +26,27 @@ class Drone:
         self.multiranger = Multiranger(scf)
     
 
-    def start_drone():
+    def start_drone(height = 0.3, velocity = 0.2):
     """has the drone take off"""
         # drone takes off
-        self.motionCommander.take_off(height=self.height, velocity=self.velocity)
+        self.motionCommander.take_off(height = height, velocity = velocity)
 
-
+    def land_drone(self):
+        self.motionCommander.land()
     
-    def move(direction):
+    def move(direction, vector):
     """moves the drone at a constant speed in one direction"""
         if direction is Direction.UP:
             print "move up"
-        if direction is Direction.DOWN:
+        elif direction is Direction.DOWN:
             print "move down"
-        if direction is Direction.LEFT:
+        elif direction is Direction.LEFT:
             print "move left"
-        if direction is Direction.RIGHT:
+        elif direction is Direction.RIGHT:
             print "move right"
-        if direction is Direction.FOWARDS:
+        elif direction is Direction.FOWARDS:
             print "move fowards"
-        if direction is Direction.BACKWARDS:
+        elif direction is Direction.BACKWARDS:
             print "move backwards"
         else:
             print "Invalid command"
